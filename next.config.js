9e1f8e737e66e805f643bb98547b58dcb27221f7
drazenbuljovcic <drazen.buljovcic@sumup.com>
2021-09-27 12:20:27 +0200

    working example

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+    localeDetection: false,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/es-US/:path*",
+        destination: "/en-US/:path*",
+        locale: false,
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/es-US/:path*",
+        destination: "/en-US/:path*",
+        permanent: true,
+        locale: false,
+      },
+    ];
+  },
+};
